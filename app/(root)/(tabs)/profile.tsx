34a2b96b3d3a1926355f85cc0ccfcab2f7e9d585
Benson <bgerald30@gmail.com>
2024-10-14 16:28:07 +0300

    Configuration, file and folder structure and router setup

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
+const Profile = () => {
+  return (
+    <SafeAreaView>
+      <Text>Profile</Text>
+    </SafeAreaView>
+  );
+};
+export default Profile;
