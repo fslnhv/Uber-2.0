ef5b4bd42518ba8cf07b53789b47211595118f48
Benson <bgerald30@gmail.com>
2024-10-15 22:11:45 +0300

    Added conditional rendering in home file

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,10 +1,25 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
+  const { user } = useUser();
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <View>
+        <SignedIn>
+          <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        </SignedIn>
+        <SignedOut>
+          <Link href="../../(auth)/sign-in.tsx">
+            <Text>Sign In</Text>
+          </Link>
+          <Link href="../../(auth)/sign-up.tsx">
+            <Text>Sign Up</Text>
+          </Link>
+        </SignedOut>
+      </View>
     </SafeAreaView>
   );
 };
