a5cf9ad35cbe214aaad23e22a99045c6d160df29
Benson <bgerald30@gmail.com>
2024-10-15 22:10:20 +0300

    Added conditional rendering in index file

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,10 @@
 import { Redirect } from "expo-router";
 const Home = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href={"/(root)/(tabs)/home"} />;
+  }
   return <Redirect href="/(auth)/welcome" />;
 };
 export default Home;
