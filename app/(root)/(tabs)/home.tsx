027530b8385d95cec37a6d61973c2c48ea3b92e7
Benson <bgerald30@gmail.com>
2024-10-16 12:43:10 +0300

    Added tab screens in the layout and made a TabIcon component

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,25 +1,14 @@
-import { Text, View } from "react-native";
-import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
+import { Text } from "react-native";
+import { SignedIn, useUser } from "@clerk/clerk-expo";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   const { user } = useUser();
   return (
     <SafeAreaView>
-      <View>
-        <SignedIn>
-          <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        </SignedIn>
-        <SignedOut>
-          <Link href="../../(auth)/sign-in.tsx">
-            <Text>Sign In</Text>
-          </Link>
-          <Link href="../../(auth)/sign-up.tsx">
-            <Text>Sign Up</Text>
-          </Link>
-        </SignedOut>
-      </View>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
     </SafeAreaView>
   );
 };
