590879955b06d36943fc5dd1028847a4cd48e459
Benson <bgerald30@gmail.com>
2024-10-19 22:47:10 +0300

    Made confirm ride bottom slider screen

--- app/(root)/(tabs)/confirm-ride.tsx
+++ app/(root)/(tabs)/confirm-ride.tsx
@@ -1,10 +1,34 @@
-import { View, Text } from "react-native";
+import { View, FlatList } from "react-native";
+import RideLayout from "@/components/RideLayout";
+import DriverCard from "@/components/DriverCard";
+import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
 
 const ConfirmRide = () => {
+  const { drivers, selectedDriver, setSelectedDriver } = useDriverStore();
+
   return (
-    <View>
-      <Text>Confirm Ride</Text>
-    </View>
+    <RideLayout title={"Choose a Rider"} snapPoints={["65%", "85%"]}>
+      <FlatList
+        data={drivers}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <DriverCard
+            item={item}
+            selected={selectedDriver!}
+            setSelected={() => setSelectedDriver(item.id!)}
+          />
+        )}
+        ListFooterComponent={() => (
+          <View className="mx-5 mt-10">
+            <CustomButton
+              title="Select Ride"
+              onPress={() => router.push("/(root)/book-ride")}
+            />
+          </View>
+        )}
+      />
+    </RideLayout>
   );
 };
 export default ConfirmRide;
